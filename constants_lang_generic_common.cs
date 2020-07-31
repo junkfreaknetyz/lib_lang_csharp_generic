1b89977833a9fa9a0bbe9f3cc636d93ce7534461
junkfreaknetyz <junk.freak.net.yz@gmail.com>
2020-08-01 01:00:15 +0900

    first commit 2020/07/31 01:00

---
+++ constants_lang_generic_common.cs
@@ -0,0 +1,7 @@
+namespace language.generic.common {
+    public static class constants {
+    public static readonly string CS_SPACE="";
+    public static readonly int CS_ZERO_AS_INT=0;
+    public static readonly string CS_ZERO_AS_STRING="0";
+    }   
+}
